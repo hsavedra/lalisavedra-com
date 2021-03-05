5822f808902b0a8e4b01660f00a04d169d3e5484
Savedra <heber.savedra@gmail.com>
2021-03-04 21:58:35 -0300

    replace http://lalisavedra.com/, part II

--- lalisavedra.com/xmlrpc0db0.php
+++ lalisavedra.com/xmlrpc0db0.php
@@ -8,7 +8,7 @@
 			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://lalisavedra.com/xmlrpc.php" />
 			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://lalisavedra.com/xmlrpc.php" />
 			<api name="Blogger" blogID="1" preferred="false" apiLink="https://lalisavedra.com/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://lalisavedra.com/wp-json/" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://lalisavedra.com/lalisavedra.com/wp-json/" />
 			</apis>
 	</service>
 </rsd>
